1681c9cf353d8e6f2ece7a614e71ec771c768d8b
Kamal ELMAGOURI <kamalELMAGOURI@gmail.com>
2025-03-15 16:03:48 +0000

    add login_page

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"edd8546116457bdf1c5bdfb13ecb9463d2bb5e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1626308572"
+    serviceWorkerVersion: "2255426560"
   }
 });
